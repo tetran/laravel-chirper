63517458c77aad21b45b6ccb41074acb21de99e3
Koichi Kaneshige <coarse.ground@gmail.com>
2026-01-25 15:52:01 +0900

    update / delete chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -33,4 +33,30 @@ class ChirpController extends Controller
 
         return redirect('/')->with('success', 'Your chirp has been posted!');
     }
+
+    public function edit(Chirp $chirp)
+    {
+        // We'll add authorization in lesson 11
+        return view('chirps.edit', compact('chirp'));
+    }
+
+    public function update(Request $request, Chirp $chirp)
+    {
+        // Validate
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        // Update
+        $chirp->update($validated);
+
+        return redirect('/')->with('success', 'Chirp updated!');
+    }
+
+    public function destroy(Chirp $chirp)
+    {
+        $chirp->delete();
+
+        return redirect('/')->with('success', 'Chirp deleted!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
 
 Route::get('/', [ChirpController::class, 'index']);
-Route::post('/chirps', [ChirpController::class, 'store']);
+Route::resource('chirps', ChirpController::class)
+    ->only(['store', 'edit', 'update', 'destroy']);
